109164d841f42bdcfa294b48d62cdeebfe02a252
yanchixia <35692289+zyanchixia@users.noreply.github.com>
2018-03-07 21:32:08 +0800

    002比较运算符

---
+++ 基础语法/002比较运算符.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  7 14:47:47 2018
+
+"""
+'''python比较运算符
+==,!=,<>,>,<,>=,<=
+'''
+a,b,c = 21,10,0
+
+if(a==b):
+    print('1-a等于b')
+else:
+    print('1-a不等于b') #1-a不等于b
+
+if(a!=b):
+    print('2-a不等于b')
+else:
+    print('2-a等于b')#2-a不等于b
+
+if(a<b):
+    print('3-a小于b')
+else:
+    print('3-a大于等于b')#3-a大于等于b
+    
+
+
+
+

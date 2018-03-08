203719fa774675f675a6eb9a2ecd49096b74ef9e
yanchixia <35692289+zyanchixia@users.noreply.github.com>
2018-03-08 21:02:21 +0800

    005三角函数

---
+++ 基础语法/005python三角函数.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  8 14:08:10 2018
+
+"""
+'''
+python三角函数
+'''
+
+
+#函数       	描述
+acos(x)	#返回x的反余弦弧度值。
+asin(x)	#返回x的反正弦弧度值。
+atan(x)	#返回x的反正切弧度值。
+atan2(y, x)	#返回给定的 X 及 Y 坐标值的反正切值。
+cos(x)	       #返回x的弧度的余弦值。
+hypot(x, y)	#返回欧几里德范数 sqrt(x*x + y*y)。
+sin(x)	   #返回的x弧度的正弦值。
+tan(x)	   #返回x弧度的正切值。
+degrees(x)	   #将弧度转换为角度,如degrees(math.pi/2) ， 返回90.0
+radians(x)	   #将角度转换为弧度

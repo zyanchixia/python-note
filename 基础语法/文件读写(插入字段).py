e0f0ba947d3e45c7c21cd6bec37f41c0ca5b2fec
yanchixia <35692289+zyanchixia@users.noreply.github.com>
2018-03-31 13:03:25 +0800

    文件读取（插入字段）

--- 基础语法/文件读写(插入字段).py
+++ 基础语法/文件读写(插入字段).py
@@ -10,7 +10,7 @@ file_obj.seek(offset,whence=0),在文件中移动文件指针，
 whence参数可选，默认为0
 '''
 s = 'Today is a good day,do you think so?'
-with open (r'C:\Users\ningkang\Desktop\companies.txt','a+') as f:
+with open ('cs.txt','a+') as f:
     #a+以读和追加模式打开
     f.writelines('\n')
     f.writelines(s)

3bd8a401f3a622f9e366a0dea2ae7534d262d678
yanchixia <35692289+zyanchixia@users.noreply.github.com>
2018-04-07 09:56:59 +0800

    PythonDraw

--- 程序语言设计/PythonDraw.py
+++ 程序语言设计/PythonDraw.py
@@ -2,17 +2,17 @@
 import turtle
 turtle.setup(650,350,200,200)
 turtle.penup() #拿起笔
-turtle.fd(-250)
+turtle.fd(-250)#倒退前进250
 turtle.pendown() #放下笔
 turtle.pensize(25) #画笔型号
 turtle.pencolor('blue') #画笔颜色
-turtle.seth(-40)#控制海龟绝对方向
-for i in range(4):
+turtle.seth(-40)#将海龟方向指向坐标系中绝对40方向 
+for i in range(4):#产生循环计数序列
     turtle.circle(40,80)#以40为半径，80度为弧度
     turtle.circle(-40,80)
 turtle.circle(40,80/2)
-turtle.fd(40)
+turtle.fd(40)#向前行进40，脖子部分
 turtle.circle(16,180)
 turtle.fd(40*2/3)
-turtle.done()
+turtle.done()#去掉，则运行完自动关闭
 

b1c8a3f2681b86a5130f475327b4693b5b1bdad8
yanchixia <35692289+zyanchixia@users.noreply.github.com>
2018-04-07 15:05:39 +0800

    DayDayupQ3工作日的力量

--- 程序语言设计/DayDayupQ3.py
+++ 程序语言设计/DayDayupQ3.py
@@ -4,7 +4,7 @@
 dayup = 1.0
 dayfactor = 0.01
 for i in range(365):
-    if 1 % 7 in [6,0]:#每周7天，余数为6或0时表示双休日
+    if i % 7 in [6,0]:#每周7天，余数为6或0时表示双休日
         dayup = dayup*(1-dayfactor)#双休日退步1%
     else:
         dayup = dayup*(1+dayfactor)#工作日进步1%

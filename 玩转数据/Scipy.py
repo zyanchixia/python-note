a3c4360ad2bdd5408a654336f40fe5229951859e
yanchixia <35692289+zyanchixia@users.noreply.github.com>
2018-04-10 21:58:28 +0800

    小栗子

---
+++ 玩转数据/Scipy.py
@@ -0,0 +1,12 @@
+#课程中的例题
+from pandas import Series
+data = {'Scala': ____, 'Java': 1995, 'Python': 1991, '___': 2009}
+ser = Series(data)
+'C' in ser
+False
+'Go' in ser
+True
+ser.values.mean() #计算平均数
+1999.5
+
+#输出结果为  2003;Go
